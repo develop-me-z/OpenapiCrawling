25f2ddc3354752fd8c35932b2607d9664c13c599
Jisoo <ohjisoo826@gmail.com>
2023-02-07 23:41:47 +0900

    JPA 적용

---
+++ src/main/java/com/crawl/openapi/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.crawl.openapi.service.posts;
+
+import com.crawl.openapi.domain.posts.PostsRepository;
+import com.crawl.openapi.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
