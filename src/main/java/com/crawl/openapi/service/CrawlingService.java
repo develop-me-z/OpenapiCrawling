9a6eaff2a12c518dc0c9dc3194235f6f606c6f1d
Jisoo <ohjisoo826@gmail.com>
2023-02-16 22:55:19 +0900

    CSV 다운로드 구현

--- src/main/java/com/crawl/openapi/service/CrawlingService.java
+++ src/main/java/com/crawl/openapi/service/CrawlingService.java
@@ -1,31 +1,21 @@
 package com.crawl.openapi.service;
 
-import com.crawl.openapi.web.dto.CrawlingRequestDto;
+import com.opencsv.CSVWriter;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Node;
 
 import javax.annotation.PostConstruct;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 @Service
 public class CrawlingService {
-
-    //private static String RfW1_URL = "http://www.wamis.go.kr:8080/wamis/openapi/wkw/rf_dubrfobs";
 
     @PostConstruct
     public String getRfW1Data() throws Exception {
@@ -49,17 +39,15 @@ public class CrawlingService {
         Crawler(result.toString());
 
         return result.toString();
-
-        //Document doc = Jsoup.connect(RfW1_URL).ignoreContentType(true).get();
-        //Elements contents = doc.select("body");
-        // Crawler(doc);
-        // System.out.println(track.size());
     }
 
     public void Crawler(String str) throws Exception {
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = (JSONObject) jsonParser.parse(str);
         JSONArray lists = (JSONArray) jsonObject.get("list");
+
+        CSVWriter writer = new CSVWriter(new FileWriter("./src/main/resources/static/csv/rfw1.csv"));
+
         for(int i=0; i<lists.size(); i++){
             JSONObject listbody= (JSONObject) lists.get(i);
 
@@ -73,8 +61,11 @@ public class CrawlingService {
 
             //todo
             // 엑셀 다운로드 만들기
-            //CrawlingRequestDto dto = new CrawlingRequestDto(bbsnnm, obscd, obsnm, clsyn, obsknd, sbsncd, mngorg);
+            // CrawlingRequestDto dto = new CrawlingRequestDto(bbsnnm, obscd, obsnm, clsyn, obsknd, sbsncd, mngorg);
+            // CSVWriterBuilder builder = new CSVWriterBuilder(dto);
+            writer.writeNext(new String[] {bbsnnm, obscd, obsnm, clsyn, obsknd, sbsncd, mngorg});
         }
+        writer.close();
     }
 
 
