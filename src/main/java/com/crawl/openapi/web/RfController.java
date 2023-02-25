bb41b318d588089e0ec3eba38881eb8e93a99c8c
Jisoo <ohjisoo826@gmail.com>
2023-02-25 22:08:21 +0900

    데이터 뷰단으로 가져오기 csv로 다운로드 기능 구현

--- src/main/java/com/crawl/openapi/web/RfController.java
+++ src/main/java/com/crawl/openapi/web/RfController.java
@@ -8,10 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.util.List;
@@ -33,13 +31,17 @@ public class RfController {
         return "rf/w1";
     }
 
-    @SneakyThrows
+    //@ResponseBody
     @PostMapping("/search/w1")
     public String searchRfW1Data(Model model, @RequestBody CrawlingRequestDto param) throws Exception {
 
+        ModelAndView mav = new ModelAndView("rf/w1");
+
         List<CrawlingRequestDto> crawlingRequestDtoList = crawlingService.getRfW1Data(param);
+        //mav.addObject("data", crawlingRequestDtoList);
         model.addAttribute("data", crawlingRequestDtoList);
 
+        //return model;
         return "rf/w1";
     }
 
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -32,34 +32,46 @@ var main = {
         $.ajax({
             type: 'POST',
             url: '/rf/search/w1',
-            dataType: 'json',
+            //dataType: 'json',
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data),
-            success: function(data){
+            beforeSend : function(){
+              //location.reload();
 
+            },
+            success: function (result){
+                //alert(result);
+                document.write(result);
             }
         }).done(function() {
-            alert('글이 등록되었습니다.');
-            window.location.href = '/';
+
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
     save : function () {
-        var data = {};
+        //CSV 문자열 생성
+        let downLink = document.getElementById('btnSave');
+        let csv = ''; //CSV최종 문자열을 저장하는 변수
+        let rows = document.querySelectorAll("#dataTable tr"); // 테이블에서 행 요소들을 모두 선택
+        let fileName = '강수량관측소.csv';
 
-        $.ajax({
-            type: 'POST',
-            url: '/rf/save/w1',
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 등록되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
+        //행단위 루핑
+        for (var i = 0; i < rows.length; i++) {
+            let cells = rows[i].querySelectorAll("td, th");
+            let row = [];
+            //행의 셀 값을 배열로 얻기
+            cells.forEach(function(cell){
+                row.push(cell.innerHTML);
+            });
+
+            csv += row.join(',') + (i != rows.length-1 ? '\n':''); // 배열을 문자열+줄바꿈으로 변환
+        }
+
+        //CSV 파일 저장
+        csvFile = new Blob([csv], {type: "text/csv"}); // 생성한 CSV 문자열을 Blob 데이터로 생성
+        downLink.href = window.URL.createObjectURL(csvFile); // Blob 데이터를 URL 객체로 감싸 다운로드 하이퍼링크에 붙임.
+        downLink.download = fileName; // 인자로 받은 다운로드 파일명을 지정
     },
     update : function () {
         var data = {
